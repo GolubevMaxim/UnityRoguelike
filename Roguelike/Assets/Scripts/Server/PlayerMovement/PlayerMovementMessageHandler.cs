113439f9e7e3361c8984791ce464037e6def3a6e
GolubevMaxim <vfrc008@icloud.com>
2022-03-24 12:41:55 +0300

    move message handler added

---
+++ Roguelike/Assets/Scripts/Server/PlayerMovement/PlayerMovementMessageHandler.cs
@@ -0,0 +1,44 @@
+﻿using RiptideNetworking;
+using UnityEngine;
+
+namespace Server.PlayerMovement
+{
+    public class PlayerMovementMessageHandler : MonoBehaviour
+    {
+        [MessageHandler((ushort) NetworkManager.ClientToServerId.Dash)]
+        private static void GetDashRequest(ushort playerId, Message message)
+        {
+            var dashDirection = message.GetVector2();
+            
+            Players.Dictionary.TryGetValue(playerId, out var player);
+            if (player != null)
+            {
+                player.GetComponent<Server.PlayerMovement.PlayerMovement>().Dash(dashDirection);
+            }
+        }
+        
+        [MessageHandler((ushort) NetworkManager.ClientToServerId.DirectionInput)]
+        private static void GetSpawnPlayerMessage(ushort playerId, Message message)
+        {
+            var moveDirection = message.GetVector2();
+            Players.Dictionary.TryGetValue(playerId, out var player);
+            if (player == null) return;
+            
+            player.GetComponent<Server.PlayerMovement.PlayerMovement>().Move(moveDirection);
+            SendPosition(playerId);
+        }
+
+        private static void SendPosition(ushort playerId)
+        {
+            Players.Dictionary.TryGetValue(playerId, out var player);
+            if (player == null) return;
+
+            var message = Message.Create(MessageSendMode.unreliable, NetworkManager.ServerToClientId.PlayerPositionChange);
+            
+            message.AddUShort(playerId);
+            message.AddVector3(player.transform.position);
+            
+            NetworkManager.Singleton.Server.SendToAll(message);
+        }
+    }
+}
